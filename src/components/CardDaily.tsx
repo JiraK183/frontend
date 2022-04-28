80ad4d82eabb74bf54659913a568952921ff76d2
Lukasket <79446355+Lukasket@users.noreply.github.com>
2022-04-28 20:59:04 +0300

    added modal on how to claim daily reward

--- src/components/CardDaily.tsx
+++ src/components/CardDaily.tsx
@@ -1,13 +1,23 @@
-import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme, Modal } from '@mantine/core';
+import { useState } from 'react';
 
 function DailyCard() {
   const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
 
   const secondaryColor = theme.colorScheme === 'dark'
     ? theme.colors.dark[1]
     : theme.colors.gray[7];
 
   return (
+    <>
+    <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="To claim daily reward need to make any action in Jira (Ex: replace task from 'IN PROGRESS' to 'DONE')"
+      >
+        {/* Modal content */}
+      </Modal>
     <div style={{ margin: 'auto' }}>
       <Card shadow="sm" p="lg" style={{ height: 350, width: 400}}>
         <Text weight={500} size="lg" >
@@ -16,11 +26,12 @@ function DailyCard() {
         <Text size="md" style={{ color: secondaryColor, lineHeight: 1.5 }}>
           Not claimed yet!
         </Text>
-        <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+        <Button onClick={() => setOpened(true)} variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
           How to claim?
-        </Button>
+        </Button>       
       </Card>
     </div>
+    </>
   );
 }
 
